508feab5081d70c0ea77b60d7554ddc0dca0fe4a
zadid15 <muhammadzadid061512@gmail.com>
2025-02-23 19:21:56 +0700

    add models and migrations for class cash system

---
+++ KasKelasLaravel/database/migrations/2025_02_23_121806_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('transaction_id')->constrained();
+            $table->string('proof_image');
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
